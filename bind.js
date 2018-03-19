3a7b00f4ba0839a36bff415bff84a34a52629023
Shi Tuocheng <shituocheng@126.com>
2018-03-19 16:46:30 +0800

    first commit

---
+++ bind.js
@@ -0,0 +1,49 @@
+/*jshint esversion: 6 */
+
+function cd(value) {
+  console.log('更新世图咯');
+}
+
+function defineReactive(obj, key, val){
+  Reflect.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    set: function reactiveSetter(newVal){
+      if(val !== newVal) cd(newVal);
+    },
+    get: function reactiveGetter (){
+      return val;
+    }
+  });
+}
+
+function observable(value) {
+  if(!value || (typeof value !== 'object')){
+    return;
+  }
+
+  // console.log(Object.key(value));
+
+  Object.keys(value).forEach((key) => {
+    console.log(value);
+    console.log(key);
+    defineReactive(value, key, value[key]);
+  })
+}
+
+class Vue {
+  constructor(options){
+    this._data = options.data;
+    observable(this._data);
+  }
+}
+
+const vue = new Vue(
+  {
+    data: {
+      test: 'initial Data'
+    }
+  }
+);
+
+vue._data.test = "i change the data!";
